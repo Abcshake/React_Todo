66e62b1e0edccb8d355b7bbd89d738736fe5542d
Avishek <avishiek@gmail.com>
2020-11-02 19:42:46 -0500

    add slider

---
+++ src/Slider.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+import { Button } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    width: 300,
+  },
+});
+
+function valuetext(value) {
+  return `${value}°C`;
+}
+
+export default function DiscreteSlider() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Typography id="discrete-slider" gutterBottom>
+       Happiness Scale
+      </Typography>
+      <Slider
+        defaultValue={30}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider"
+        valueLabelDisplay="auto"
+        step={10}
+        marks
+        min={10}
+        max={110}
+      />
+      <Button variant="contained" color="primary" type="sumbit">
+               Submit Please :)
+      </Button>
+    </div>
+  );
+}
