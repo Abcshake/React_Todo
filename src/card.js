f374a95eba08c1efa7b92b9b0708456fedc14492
Avishek <avishiek@gmail.com>
2020-11-25 23:12:48 -0500

    unsuccessfull api integration

--- src/card.js
+++ src/card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -9,29 +9,48 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 
-const useStyles = makeStyles({
+const useStyles = withStyles({
     root: {
         maxWidth: 345,
     }
 })
 
+var url = "https://api.wordnik.com/v4/word.json/"
+var word = "charming"
+var url2 = "/examples?includeDuplicates=false&useCanonical=false&limit=5&api_key=8xjha7s9g4047yn5nu0v8pvlasuazkl4yaqt72csvkepbf161"
 
-export default function MediaCard(){
-    const classes = useStyles
+
+
+class MediaCard extends React.Component{
+    //const classes = useStyles
+
+    state= {
+        word: null,
+        example: null
+    };
+
+    async componentDidMount(){
+        const finalUrl = url + word + url2;
+        const response = await fetch(finalUrl);
+        const data = await response.json();
+        this.setState({word: data.examples[0].provider.word,
+                        example: data.examples[0].provider.text });
+    }
+
+    render(){
     return ( 
-        <Card className={classes.root}>
+        <Card>
             <CardActionArea>
                 <CardMedia
-                    className={classes.media}
                     image="/src/images/stoa_poikile.jpg"
                     title="Stoic Porch"
                 />
                 <CardContent>
                     <Typography variant="h5" component="h2">
-                        Stoia Poikile
+                        {this.state.word}
                     </Typography>
                     <Typography variant="body2" color="textSecondary" component="p">
-                        The painted porch at Ancient Athens where the philosopher Zeno of Critium taught Stoicism   
+                        {this.state.example}
                     </Typography>
                 </CardContent>
             </CardActionArea>
@@ -45,4 +64,7 @@ export default function MediaCard(){
             </CardActions>
         </Card>
     )
+    }
 }
+
+export default MediaCard;
