018ced9d9124cfa32759bf6a271abda3d7e25b9a
Avishek <avishiek@gmail.com>
2020-11-26 23:23:05 -0500

    fixed Card issue

--- src/card.js
+++ src/card.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
+//import { useStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+// import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+//import Paper from '@material-ui/core/Paper';
 
-
-const useStyles = withStyles({
-    root: {
-        maxWidth: 345,
-    }
-})
+// const useStyles = withStyles({
+//     root: {
+//         maxWidth: 345,
+//     }
+// })
 
 var url = "https://api.wordnik.com/v4/word.json/"
-var word = "charming"
+var word = "Random"
 var url2 = "/examples?includeDuplicates=false&useCanonical=false&limit=5&api_key=8xjha7s9g4047yn5nu0v8pvlasuazkl4yaqt72csvkepbf161"
 
 
@@ -26,43 +26,73 @@ class MediaCard extends React.Component{
 
     state= {
         word: null,
-        example: null
+        example: null,
+        loading: true
     };
 
     async componentDidMount(){
         const finalUrl = url + word + url2;
         const response = await fetch(finalUrl);
         const data = await response.json();
-        this.setState({word: data.examples[0].provider.word,
-                        example: data.examples[0].provider.text });
+        this.setState({word: data.examples[0].word,
+                        example: data.examples[0].text, loading:false });
     }
 
     render(){
+        if (this.state.loading) {
+            return <div>loading...</div>;
+          }
+      
+          if (!this.state.word&&!this.state.example) {
+            return <div>didn't find anything</div>;
+          }
     return ( 
+        <div>
         <Card>
             <CardActionArea>
-                <CardMedia
-                    image="/src/images/stoa_poikile.jpg"
-                    title="Stoic Porch"
-                />
-                <CardContent>
-                    <Typography variant="h5" component="h2">
-                        {this.state.word}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {this.state.example}
-                    </Typography>
-                </CardContent>
+            <CardContent>
+                <Typography variant="h5" component="h2">
+                          {this.state.word}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                         {this.state.example}
+                </Typography>
+            </CardContent>
             </CardActionArea>
             <CardActions>
-                <Button size="small" color="primary">
-                    Share
-                </Button>
-                <Button size="small" color="secondary">
-                    Like
-                </Button>
-            </CardActions>
+                 <Button size="small" color="primary">
+                     Share
+                 </Button>
+                 <Button size="small" color="secondary">
+                     Like
+                 </Button>
+             </CardActions>
         </Card>
+        </div>
+        // <Card>
+        //     <CardActionArea>
+        //         <CardMedia
+        //             image="/src/images/stoa_poikile.jpg"
+        //             title="Stoic Porch"
+        //         />
+        //         <CardContent>
+        //             <Typography variant="h5" component="h2">
+        //                 {this.state.word}
+        //             </Typography>
+        //             <Typography variant="body2" color="textSecondary" component="p">
+        //                 {this.state.example}
+        //             </Typography>
+        //         </CardContent>
+        //     </CardActionArea>
+        //     <CardActions>
+        //         <Button size="small" color="primary">
+        //             Share
+        //         </Button>
+        //         <Button size="small" color="secondary">
+        //             Like
+        //         </Button>
+        //     </CardActions>
+        // </Card>
     )
     }
 }
